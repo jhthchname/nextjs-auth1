0cadfad87f4664c2dcb521f0e09292ac4aa1f795
Jhinthicha Sukchaoritthikrai <namelie34@gmail.com>
2024-07-15 22:44:52 +0700

    updated files

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -102,7 +102,7 @@ export default function Login() {
             </button>
           </form>
           <div className="mt-5 text-sm flex justify-center items-center">
-            <p className="text-gray-700 ">Don't have an account?</p>
+            <p className="text-gray-700 ">Don&apost have an account?</p>
             <a
               href="/signup"
               className="text-[#6e59e7] font-bold ml-2 hover:underline"
