a355b82c30a84351b370e0a66652d8c58d234a32
Jhinthicha Sukchaoritthikrai <namelie34@gmail.com>
2024-07-25 09:27:18 +0700

    updated

--- server/route/auth.route.js
+++ server/route/auth.route.js
@@ -1,54 +1,29 @@
 import express from "express";
-import formController from "../controller/form.controller.js";
-import { verifyToken } from "../auth/auth.service.js";
+import authController from "../controller/auth.controller.js";
+import { setAuthCookie, verifyToken } from "../auth/auth.service.js";
 
-const form = express.Router();
+const authRoute = express.Router();
 
-form
-  .get("/", async (req, res) => {
+authRoute
+  .post("/signin", async (req, res) => {
     try {
-      await verifyToken(req?.headers?.authorization);
-      let newUser = await formController.forms(req.query);
-      res.status(200).json(newUser);
+      let result = await authController.signin(req.body);
+      setAuthCookie(res, result);
+      res.status(200).json(result);
     } catch (error) {
-      res.status(400).json({ message: error?.message });
+      res.status(400).json({ message: error.message });
     }
   })
-  //   .get("/:id", async (req, res) => {
-  //     try {
-  //       console.log("req id========>", req.params);
-  //       let newUser = await userController.user(req.params);
-  //       res.status(200).json(newUser);
-  //     } catch (error) {
-  //       res.status(400).json({ message: error?.message });
-  //     }
-  //   })
-  .post("/create", async (req, res) => {
+  .post("/signout", async (req, res) => {
     try {
       const user = await verifyToken(req?.headers?.authorization);
-      let result = await formController.create(req?.body, user);
+      let result = await authController.signout(user);
+      res.clearCookie("user");
+      res.clearCookie("token");
       res.status(200).json(result);
     } catch (error) {
-      res.status(400).json({ message: error?.message });
-    }
-  })
-  .put("/update", async (req, res) => {
-    try {
-      const user = await verifyToken(req?.headers?.authorization);
-      let result = await formController.update(req?.body, user);
-      res.status(200).json(result);
-    } catch (error) {
-      res.status(400).json({ message: error?.message });
-    }
-  })
-  .delete("/:id", async (req, res) => {
-    try {
-      await verifyToken(req?.headers?.authorization);
-      let result = await formController.delete(req?.params);
-      res.status(200).json(result);
-    } catch (error) {
-      res.status(400).json({ message: error?.message });
+      res.status(400).json({ message: error.message });
     }
   });
 
-export default form;
+export default authRoute;
