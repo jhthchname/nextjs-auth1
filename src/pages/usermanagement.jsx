0c31a60ba68cc06dc045b002bf68fd20340a17a4
Jhinthicha Sukchaoritthikrai <namelie34@gmail.com>
2024-07-25 18:00:52 +0700

    updated

--- src/pages/usermanagement.jsx
+++ src/pages/usermanagement.jsx
@@ -1,51 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import LayoutContainer from "../components/Layout";
 import ModalEdit from "../components/Modal";
 import { FaSearch } from "react-icons/fa";
-import { Button, Modal } from "antd";
+import { Button, Modal, Pagination, Spin } from "antd";
 import { MdEdit, MdDeleteForever } from "react-icons/md";
 import { checkAuth } from "@/auth/common";
+import axios from "axios";
 
 export async function getServerSideProps({ req }) {
   return await checkAuth(req);
 }
 
 export default function UserManagement(props) {
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      firstName: "Roseanne",
-      lastName: "Park",
-      email: "rose@example.com",
-      phone: "0801234567",
-    },
-    {
-      id: 2,
-      firstName: "Pansa",
-      lastName: "Vosbein",
-      email: "milk@example.com",
-      phone: "0809876543",
-    },
-    {
-      id: 3,
-      firstName: "Manichar",
-      lastName: "Aimdilokwong",
-      email: "marmink@example.com",
-      phone: "0812345678",
-    },
-  ]);
-
+  const [users, setUsers] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedUser, setSelectedUser] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  let defaultPageSize = 10;
 
-  const filteredUsers = users.filter(
-    (user) =>
-      user.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      user.lastName.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      user.email.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      user.phone.includes(searchTerm)
-  );
+  useEffect(() => {
+    setLoading(true);
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get(
+          `/api/user?query=${searchTerm}&skip=${
+            (currentPage - 1) * defaultPageSize
+          }`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              authorization: `Bearer ${props.user.token}`,
+            },
+          }
+        );
+
+        const mapUser = response.data.results.map((user) => ({
+          id: user._id,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email || "-",
+          phone: user.phone || "-",
+        }));
+        setUsers({ data: mapUser, total: response.data.total });
+        setLoading(false);
+      } catch (error) {
+        setUsers([]);
+        setLoading(false);
+      }
+    };
+    fetchUsers();
+  }, [searchTerm, currentPage]);
 
   const handleEdit = (user) => {
     setSelectedUser(user);
@@ -92,57 +98,78 @@ export default function UserManagement(props) {
                 type="text"
                 placeholder="Search..."
                 value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
+                onChange={(e) => {
+                  setSearchTerm(e.target.value);
+                  setCurrentPage(1);
+                }}
                 className="w-full px-3 py-2 pl-10 border rounded focus:outline-none focus:border-[#6e59e7] focus:ring-[#806aff] block focus:ring-1"
               />
               <FaSearch className="absolute left-3 top-3 text-gray-500" />
             </div>
           </div>
-          <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
-            <table className="w-full text-sm text-gray-500">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50">
-                <tr className="bg-[#e6e1ff]">
-                  <th className="border p-2">First Name</th>
-                  <th className="border p-2">Last Name</th>
-                  <th className="border p-2">Email</th>
-                  <th className="border p-2">Phone number</th>
-                  <th className="border p-2">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {filteredUsers.map((user) => (
-                  <tr key={user.id}>
-                    <td className="border p-2">{user.firstName}</td>
-                    <td className="border p-2">{user.lastName}</td>
-                    <td className="border p-2">{user.email}</td>
-                    <td className="border p-2">{user.phone}</td>
-                    <td className="border p-2">
-                      <div className="flex justify-center space-x-2">
-                        <Button
-                          onClick={() => handleEdit(user)}
-                          className="bg-[#e6e1ff] px-3 py-2 rounded-sm hover:bg-[#cfc7ff]"
-                        >
-                          <MdEdit className="fill-[#6e59e7]" />
-                        </Button>
-                        <Button
-                          onClick={() => {
-                            Modal.confirm({
-                              title:
-                                "Are you sure you want to delete this user?",
-                              onOk: () => handleDelete(user.id),
-                            });
-                          }}
-                          className="bg-[#ffdada] px-3 py-2 rounded-sm hover:bg-white"
-                        >
-                          <MdDeleteForever className="fill-[#fc4a4a]" />
-                        </Button>
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+          {loading ? (
+            <div className="w-full flex justify-center h-[208px]">
+              <Spin size="large" className="my-auto" />
+            </div>
+          ) : (
+            <>
+              <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
+                <table className="w-full text-sm text-gray-500">
+                  <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+                    <tr className="bg-[#e6e1ff]">
+                      <th className="border p-2">First Name</th>
+                      <th className="border p-2">Last Name</th>
+                      <th className="border p-2">Email</th>
+                      <th className="border p-2">Phone number</th>
+                      <th className="border p-2">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {users?.data?.map((user) => (
+                      <tr key={user.id}>
+                        <td className="border p-2">{user.firstName}</td>
+                        <td className="border p-2">{user.lastName}</td>
+                        <td className="border p-2">{user.email}</td>
+                        <td className="border p-2">{user.phone}</td>
+                        <td className="border p-2">
+                          <div className="flex justify-center space-x-2">
+                            <Button
+                              onClick={() => handleEdit(user)}
+                              className="bg-[#e6e1ff] px-3 py-2 rounded-sm hover:bg-[#cfc7ff]"
+                            >
+                              <MdEdit className="fill-[#6e59e7]" />
+                            </Button>
+                            <Button
+                              onClick={() => {
+                                Modal.confirm({
+                                  title:
+                                    "Are you sure you want to delete this user?",
+                                  onOk: () => handleDelete(user.id),
+                                });
+                              }}
+                              className="bg-[#ffdada] px-3 py-2 rounded-sm hover:bg-white"
+                            >
+                              <MdDeleteForever className="fill-[#fc4a4a]" />
+                            </Button>
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+              <div className="mt-[8px]">
+                <Pagination
+                  align="end"
+                  current={currentPage}
+                  total={users.total}
+                  defaultPageSize={defaultPageSize}
+                  onChange={(e) => setCurrentPage(e)}
+                />
+              </div>
+            </>
+          )}
+
           {isModalOpen && selectedUser && (
             <ModalEdit
               user={selectedUser}
